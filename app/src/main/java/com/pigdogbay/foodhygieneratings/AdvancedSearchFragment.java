4b1ed88729477e00f4593fa82e7b9e731bfee9b6
Mark Bailey <pigdogbay@yahoo.co.uk>
2017-03-16 16:38:57 +0000

    Advanced search menu

--- app/src/main/java/com/pigdogbay/foodhygieneratings/AdvancedSearchFragment.java
+++ app/src/main/java/com/pigdogbay/foodhygieneratings/AdvancedSearchFragment.java
@@ -4,6 +4,8 @@ package com.pigdogbay.foodhygieneratings;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -23,8 +25,14 @@ public class AdvancedSearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_advanced_search, container, false);
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_advanced_search,menu);
+    }
 }
