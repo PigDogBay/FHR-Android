f28fd19724e32edfa41569988e76fac83c6788a2
Mark Bailey <pigdogbay@yahoo.co.uk>
2017-03-24 16:44:06 +0000

    Map fragment that shows only 1 establishment

--- app/src/main/java/com/pigdogbay/foodhygieneratings/EstablishmentMapFragment.java
+++ app/src/main/java/com/pigdogbay/foodhygieneratings/EstablishmentMapFragment.java
@@ -2,30 +2,41 @@ package com.pigdogbay.foodhygieneratings;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class EstablishmentMapFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Marker;
+import com.pigdogbay.foodhygieneratings.model.Establishment;
+import com.pigdogbay.foodhygieneratings.model.MainModel;
+import com.pigdogbay.foodhygieneratings.model.MapMarkers;
+
+public class EstablishmentMapFragment extends SupportMapFragment implements OnMapReadyCallback {
+
     public static final String TAG = "establishment map";
 
+    private GoogleMap googleMap;
+    private Establishment establishment;
 
     public EstablishmentMapFragment() {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        establishment = MainModel.get(getContext()).getSelectedEstablishment();
+        getMapAsync(this);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        MapMarkers mapMarkers = new MapMarkers();
+        Marker marker = googleMap.addMarker(mapMarkers.createMarkerOptions(establishment));
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(marker.getPosition(),16.0f);
+        googleMap.moveCamera(cameraUpdate);
+    }
 }
