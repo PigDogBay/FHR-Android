eca4c104624a92d871dfd083e1997987565b30d7
Mark Bailey <pigdogbay@yahoo.co.uk>
2017-03-28 17:53:20 +0100

    Added comments about vector drawables

--- app/src/main/java/com/pigdogbay/foodhygieneratings/FHRApplication.java
+++ app/src/main/java/com/pigdogbay/foodhygieneratings/FHRApplication.java
@@ -7,10 +7,13 @@ import android.support.v7.app.AppCompatDelegate;
  * Created by Mark on 28/03/2017.
  *
  */
-
 public class FHRApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);    }
+        //need this to use vector drawables in pre-lollipop (v21) devices
+        //However there is a potential memory leak issue
+        //http://stackoverflow.com/questions/35761636/is-it-possible-to-use-vectordrawable-in-buttons-and-textviews-using-androiddraw
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 }
