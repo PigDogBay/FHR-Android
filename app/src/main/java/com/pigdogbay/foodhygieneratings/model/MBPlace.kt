ca43dad2082ef468f0d9051da73faf1d57610828
mark <mpdbailey.technology@gmail.com>
2019-03-19 14:01:33 +0000

    Place information now fetched

--- app/src/main/java/com/pigdogbay/foodhygieneratings/model/MBPlace.kt
+++ app/src/main/java/com/pigdogbay/foodhygieneratings/model/MBPlace.kt
@@ -1,14 +1,18 @@
 package com.pigdogbay.foodhygieneratings.model
 
 import android.graphics.Bitmap
-import com.google.android.gms.common.data.DataBufferUtils
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
-import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.AutocompletePrediction
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.RectangularBounds
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.api.net.FetchPlaceRequest
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
 import com.google.android.libraries.places.api.net.PlacesClient
 import com.pigdogbay.lib.patterns.ObservableProperty
 
-data class MBPlace(val id : String, val telephone : String, val web : String, val rating : Float, val images : List<IPlaceImage>)
+data class MBPlace(val id : String, val telephone : String, val web : String, val rating : Double?, val images : List<IPlaceImage>)
 enum class FetchStatus {
     Uninitialized, Fetching, Ready, Error
 }
@@ -71,7 +75,7 @@ class DummyPlace(val bitmap: Bitmap) : IPlaceFetcher {
                 val images: ArrayList<IPlaceImage> = ArrayList()
                 images.add(DummyPlaceImage("<a href=\"https://maps.google.com/maps/contrib/110455252459989089441/photos\">OMG Grill</a>", bitmap))
                 Thread.sleep(500L)
-                srcMBPlace = MBPlace("ChIJYUPe12BoekgR6iLyIiC9DJk", "07591 755084", "http://www.omg-restaurant.co.uk", 3.5F, images)
+                srcMBPlace = MBPlace("ChIJYUPe12BoekgR6iLyIiC9DJk", "07591 755084", "http://www.omg-restaurant.co.uk", 3.5, images)
                 observableStatus.value = FetchStatus.Ready
             }.start()
         }
@@ -111,8 +115,69 @@ class GooglePlaceFetcher(private val placesClient: PlacesClient) : IPlaceFetcher
     override val mbPlace: MBPlace?
         get() = foundPlace
 
+     /**
+     * To fetch a place requires 2 tasks:
+     * Fetch matching places (only first one is used)
+     * Fetch place details
+     *
+     * This information is then collated into MBPlace
+     */
     override fun fetch(establishment: Establishment) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        status.value = FetchStatus.Fetching
+        val bounds = createBounds(establishment)
+        val request = FindAutocompletePredictionsRequest.builder()
+                .setLocationRestriction(RectangularBounds.newInstance(bounds))
+                .setQuery(establishment.business.name)
+                .setTypeFilter(TypeFilter.ESTABLISHMENT)
+                .setCountry("uk")
+                .build()
+
+        //First Task is to find a list of places that match the name and location
+        placesClient.findAutocompletePredictions(request)
+                .addOnSuccessListener {
+                    response ->
+                    if (response.autocompletePredictions.size>0){
+                        fetchPlace(response.autocompletePredictions[0])
+                    } else {
+                        status.value = FetchStatus.Error
+                    }
+                }
+                .addOnFailureListener{
+                    status.value = FetchStatus.Error
+                }
+    }
+
+    private fun fetchPlace(prediction: AutocompletePrediction) {
+        val placeFields = listOf(Place.Field.NAME, Place.Field.WEBSITE_URI, Place.Field.PHONE_NUMBER, Place.Field.RATING, Place.Field.PHOTO_METADATAS)
+        val request = FetchPlaceRequest.builder(prediction.placeId,placeFields).build()
+        placesClient.fetchPlace(request)
+                .addOnSuccessListener {
+                    response ->
+                    val place = response.place
+                    val phone = if (place.phoneNumber==null) "" else place.phoneNumber.toString().replace("+44 ","0")
+                    val web = if (place.websiteUri==null) "" else place.websiteUri.toString()
+                    foundPlace = MBPlace(prediction.placeId,phone,web,place.rating,ArrayList())
+                    status.value = FetchStatus.Ready
+
+                }
+                .addOnFailureListener{
+                    status.value = FetchStatus.Error
+                }
+
+    }
+
+
+    /*
+       Returns bounds of about 0.25 mile radius about the establishment
+       1° longitude ~ 66 miles, 1° latitude ~ 69 miles, so for at least quarter mile radius
+     */
+    private fun createBounds(establishment: Establishment) : LatLngBounds {
+        val latitude = establishment.coordinate.latitude
+        val longitude = establishment.coordinate.longitude
+        val delta = 0.005
+        val northeast = LatLng(latitude + delta, longitude + delta)
+        val southwest = LatLng(latitude - delta, longitude - delta)
+        return LatLngBounds(southwest, northeast)
     }
 
 }
